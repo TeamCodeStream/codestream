13bb3cf439205246273274b0461fb7645da0f906
eric-j <etjones319@gmail.com>
2022-03-17 11:12:49 -0700

    ellipsis menu region select font size style changes

--- shared/ui/Stream/CrossPostIssueControls/IssueDropdown.tsx
+++ shared/ui/Stream/CrossPostIssueControls/IssueDropdown.tsx
@@ -588,6 +588,7 @@ export const IssueList = React.memo((props: React.PropsWithChildren<IssueListPro
 		if (initialLoadComplete) {
 			fetchCards();
 		}
+		setIsLoading(false);
 
 		return () => {
 			isValid = false;
