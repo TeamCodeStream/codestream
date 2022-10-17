fd3cdb83521dda2090d39ef5139c68b3fde6d35c
eric-j <etjones319@gmail.com>
2022-10-17 09:33:27 -0700

    removing not currently in use prRepoId derived state call from bitbucket detail page component

--- shared/ui/Stream/PullRequests/Bitbucket/PullRequest.tsx
+++ shared/ui/Stream/PullRequests/Bitbucket/PullRequest.tsx
@@ -312,7 +312,6 @@ export const PullRequest = () => {
 			textEditorUri: state.editorContext.textEditorUri,
 			reposState: state.repos,
 			checkoutBranch: state.context.pullRequestCheckoutBranch,
-			prRepoId: currentPullRequest?.conversations?.repository?.prRepoId,
 		};
 	});
 
