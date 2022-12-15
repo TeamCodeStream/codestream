81f82aeee2d0b3c42f132bd8433936a8183ad8cc
Marcelo Bukowski de Farias <mfarias@newrelic.com>
2022-12-15 17:23:46 -0500

    Compatibility with the latest IntelliJ EAP

--- jb/src/main/kotlin/commands/CodeStreamCommand.kt
+++ jb/src/main/kotlin/commands/CodeStreamCommand.kt
@@ -19,11 +19,14 @@ import org.apache.commons.io.FileUtils
 import java.io.File
 import java.net.URLDecoder
 import java.nio.charset.Charset
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Future
 
 class CodeStreamCommand : JBProtocolCommand("codestream") {
     private val logger = Logger.getInstance(CodeStreamCommand::class.java)
 
-    override fun perform(target: String?, parameters: MutableMap<String, String>) {
+    override fun perform(target: String?, parameters: MutableMap<String, String>, fragment: String?): Future<String?> {
+        val future = CompletableFuture<String?>()
         WindowFocusWorkaround.bringToFront()
 
         ApplicationManager.getApplication().invokeLater {
@@ -77,14 +80,17 @@ class CodeStreamCommand : JBProtocolCommand("codestream") {
                             if (!posted) {
                                 project.handleUrlWhenReady(project, target, parameters)
                                 posted = true
+                                future.complete(null)
                             }
                         }
                     })
             } else {
                 project.ensureOpened()
                 project.handleUrlWhenReady(project, target, parameters)
+                future.complete(null)
             }
         }
+        return future
     }
 
     private fun findOpenProject(repoMapping: RepoMapping, filePath: String): Project? {
