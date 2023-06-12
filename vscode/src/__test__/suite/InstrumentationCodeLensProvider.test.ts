8f937a809eb1f0356e495ab038e1171e24598ad9
Calvin Allen <calvinallen@newrelic.com>
2023-06-12 16:52:02 -0400

    Revert "Adding VSCode Workspace Symbol "search" method to SymbolLocator class. Needs wired up and tested."
    This reverts commit c4a50fb2e305202fa5a16767b7cbb6de568fc6c3.

--- vscode/src/__test__/suite/InstrumentationCodeLensProvider.test.ts
+++ vscode/src/__test__/suite/InstrumentationCodeLensProvider.test.ts
@@ -13,7 +13,6 @@ import {
 } from "@codestream/protocols/agent";
 
 import {
-	DocumentsLocatedBySymbols,
 	InstrumentableSymbol,
 	ISymbolLocator,
 	SymboslLocated
@@ -37,28 +36,6 @@ class MockSymbolLocator implements ISymbolLocator {
 					)
 				],
 				allSymbols: []
-			});
-		});
-	}
-
-	locateDocumentBySymbolSearch(
-		namespace: string,
-		functionName: string,
-		token: vscode.CancellationToken
-	): Promise<DocumentsLocatedBySymbols> {
-		return new Promise(resolve => {
-			resolve({
-				symbolMatches: [
-					new vscode.SymbolInformation(
-						"hello_world",
-						vscode.SymbolKind.Function,
-						"",
-						new vscode.Location(
-							vscode.Uri.parse("file:///hello/foo.py"),
-							new vscode.Range(new vscode.Position(0, 0), new vscode.Position(1, 1))
-						)
-					)
-				]
 			});
 		});
 	}
