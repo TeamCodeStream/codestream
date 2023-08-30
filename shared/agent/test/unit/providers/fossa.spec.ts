baf2c3a5b3ceb4426a700cdc3e29ca6c701bcfce
Renee Petit <renee.petit@gmail.com>
2023-08-30 14:31:49 -0700

    add in two test cases for "git+" and undefined

--- shared/agent/test/unit/providers/fossa.spec.ts
+++ shared/agent/test/unit/providers/fossa.spec.ts
@@ -28,4 +28,52 @@ describe("fossaProvider", () => {
 		);
 		expect(asdf!.id).toEqual("custom+1234/example.com/foobar/bar");
 	});
+
+	it("matchRepoToFossaProject", async () => {
+		const fossa = new FossaProvider({} as any, {} as any);
+		const asdf = await fossa._matchRepoToFossaProject(
+			{
+				currentBranch: "develop",
+				folder: {
+					uri: "file:///Users/foo/bar",
+					name: "bar",
+				},
+				path: "/Users/foo/bar",
+				providerGuess: "example",
+				id: "1234",
+			},
+			[
+				{
+					id: "git+example.com/foobar/bar",
+					title: "https://example.com/foobar/bar.git",
+				},
+			] as any,
+			"1234"
+		);
+		expect(asdf!.id).toEqual("git+example.com/foobar/bar");
+	});
+
+	it("matchRepoToFossaProject", async () => {
+		const fossa = new FossaProvider({} as any, {} as any);
+		const asdf = await fossa._matchRepoToFossaProject(
+			{
+				currentBranch: "develop",
+				folder: {
+					uri: "file:///Users/foo/bar",
+					name: "bar",
+				},
+				path: "/Users/foo/bar",
+				providerGuess: "example",
+				id: "1234",
+			},
+			[
+				{
+					id: "example.com/foobar/bar",
+					title: "https://example.com/foobar/bar.git",
+				},
+			] as any,
+			"1234"
+		);
+		expect(asdf).toEqual(undefined);
+	});
 });
