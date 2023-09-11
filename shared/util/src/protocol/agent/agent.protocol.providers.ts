b9fdbd0ec80062ac26c6a3adcb04c18d1c65fbcf
Calvin Allen <calvinallen@newrelic.com>
2023-09-11 09:28:38 -0400

    Staging filter usage

--- shared/util/src/protocol/agent/agent.protocol.providers.ts
+++ shared/util/src/protocol/agent/agent.protocol.providers.ts
@@ -2516,8 +2516,17 @@ export const DidDetectObservabilityAnomaliesNotificationType = new NotificationT
 	void
 >("codestream/didDetectObservabilityAnomalies");
 
+export interface LogRequestFilter {
+	levels?: string[];
+	codes?: string[];
+
+	missing?: string[];
+	has?: string[];
+}
+
 export interface GetLogsRequest {
 	entityGuid: string;
+	filters: LogRequestFilter;
 	order: {
 		field: string;
 		direction: string;
@@ -2544,10 +2553,8 @@ export interface GetLogFieldDefinitionsRequest {
 }
 
 export interface LogFieldDefinition {
-	stringKeys?: string[];
-	numericKeys?: string[];
-	booleanKeys?: string[];
-	allKeys?: string[];
+	key?: string;
+	type?: "string" | "boolean" | "numeric";
 }
 
 export interface GetLogFieldDefinitionsResponse {
