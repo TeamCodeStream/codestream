0de25ef6df28760ad1bbd8995f9fb02e70a6cd1c
Calvin Allen <calvinallen@newrelic.com>
2023-10-04 11:39:15 -0400

    Add message as a search option; clean up UI a little bit and add a "since" picker. Move option to view logs into Entity tree

--- shared/util/src/protocol/agent/agent.protocol.providers.ts
+++ shared/util/src/protocol/agent/agent.protocol.providers.ts
@@ -2517,6 +2517,7 @@ export const DidDetectObservabilityAnomaliesNotificationType = new NotificationT
 >("codestream/didDetectObservabilityAnomalies");
 
 export interface LogRequestFilter {
+	message?: string;
 	levels?: string[];
 	codes?: string[];
 
