0bd584c501f883e74c1c8f524f6fe10c9d48f787
Renee Petit <renee.petit@gmail.com>
2024-01-11 11:05:13 -0800

    deltaUrl added; lists deployments page on NR

--- shared/agent/src/providers/newrelic/goldenSignals/goldenSignalsProvider.ts
+++ shared/agent/src/providers/newrelic/goldenSignals/goldenSignalsProvider.ts
@@ -139,11 +139,11 @@ export class GoldenSignalsProvider {
 
 				const countResults = countResponse.actor.account.nrql?.results[0];
 
-				const permalinkUrl = countResponse.actor.entity.permalink;
-
 				const { deploymentId, timestamp } = countResults;
 
 				if (!deploymentId || !timestamp) return undefined;
+
+				const deployListUrl = this.deltaUrl(entityGuid);
 
 				const threeHoursLater = timestamp + 10800000;
 
@@ -222,7 +222,7 @@ export class GoldenSignalsProvider {
 							pillsErrorChange && getPillsSeverityColor(pillsErrorChange) ? true : false,
 						percentChange: pillsErrorChange,
 						color: pillsErrorChange && getPillsSeverityColor(pillsErrorChange),
-						permalinkUrl: permalinkUrl,
+						permalinkUrl: deployListUrl,
 					},
 					responseTimeData: {
 						isDisplayTimeResponseChange:
@@ -231,7 +231,7 @@ export class GoldenSignalsProvider {
 								: false,
 						percentChange: pillsResponseTimeChange,
 						color: pillsResponseTimeChange && getPillsSeverityColor(pillsResponseTimeChange),
-						permalinkUrl: permalinkUrl,
+						permalinkUrl: deployListUrl,
 					},
 				};
 			} catch (err) {
@@ -343,6 +343,23 @@ export class GoldenSignalsProvider {
 		}
 
 		return `${bUrl}/accounts/${accountId}/issues/${issueId}?notifierType=codestream`;
+	}
+
+	private deltaUrl(entityGuid: string) {
+		let bUrl = "";
+
+		if (this.nrApiConfig.productUrl.includes("staging")) {
+			// Staging
+			bUrl = "https://staging-one.newrelic.com";
+		} else if (this.nrApiConfig.productUrl.includes("eu")) {
+			// EU
+			bUrl = "https://one.eu.newrelic.com";
+		} else {
+			// Prod:
+			bUrl = "https://one.newrelic.com";
+		}
+
+		return `${bUrl}/nr1-core/deployment-markers/list/${entityGuid}`;
 	}
 
 	// Map array of objects based on order of array of strings
