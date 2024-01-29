63fa49ac486ce470420eb770e0ed39b42f630b24
Calvin Allen <calvinallen@newrelic.com>
2024-01-29 16:43:44 -0500

    Revertify

--- shared/ui/Stream/APMLogging/APMLogSearchPanel.tsx
+++ shared/ui/Stream/APMLogging/APMLogSearchPanel.tsx
@@ -483,7 +483,7 @@ export const APMLogSearchPanel = (props: {
 
 	return (
 		<>
-			<PanelHeader title="Logs" icon="logs">
+			<PanelHeader title="Logs">
 				<LogFilterBarContainer>
 					<div className="log-filter-bar-row">
 						<div className="log-filter-bar-service">
