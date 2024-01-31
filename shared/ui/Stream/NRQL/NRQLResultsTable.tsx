8fed0eaaee759b0be67aa1347f4a749398200520
Eric Jones <etjones319@gmail.com>
2024-01-31 12:29:55 -0800

    VariableSizeGrid with sticky header rows and different row index value sizes

--- shared/ui/Stream/NRQL/NRQLResultsTable.tsx
+++ shared/ui/Stream/NRQL/NRQLResultsTable.tsx
@@ -1,56 +1,117 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { NRQLResult } from "@codestream/protocols/agent";
+import { GridWindow } from "../GridWindow";
 
-const NRQLResultsTableRow = (props: { result: NRQLResult; columnHeaders: string[] }) => {
-	return (
-		<tr
-			style={{
-				color: "lightgray",
-				borderBottom: "1px solid lightgray",
-			}}
-		>
-			{props.result &&
-				props.columnHeaders &&
-				props.columnHeaders.map(ch => {
-					return <td>{props.result[ch]}</td>;
-				})}
-		</tr>
-	);
+const MIN_COL_WIDTH = 100;
+const MAX_COL_WIDTH = 400;
+const MIN_ROW_HEIGHT = 100;
+
+const cellStyle = {
+	wordBreak: "break-word",
+	padding: "4px",
+	borderRight: "1px solid var(--base-border-color)",
+	borderBottom: "1px solid var(--base-border-color)",
 };
 
-export const NRQLResultsTable = (props: { results: NRQLResult[] }) => {
-	const columnHeaders = Object.keys(props.results[0]);
+export const NRQLResultsTable = (props: {
+	results: NRQLResult[];
+	width: number | string;
+	height: number | string;
+}) => {
+	const hasKey = (obj, key) => {
+		return obj.hasOwnProperty(key);
+	};
 
-	const renderHeaderRow = () => {
+	const fillMissingKeys = (obj, referenceKeys) => {
+		const result = {};
+
+		referenceKeys.forEach(key => {
+			result[key] = hasKey(obj, key) ? obj[key] : ""; // Use existing value or empty string
+		});
+
+		return result;
+	};
+
+	const gridData = useMemo(() => {
+		if (!props.results || props.results.length === 0) {
+			return { columnWidths: [], columnCount: 0, resultsWithHeaders: [] };
+		}
+
+		const firstRowResults = props.results[0];
+		const filledInResults = props.results;
+		const columnCount = Object.keys(firstRowResults).length;
+		const columnHeaders = Object.keys(firstRowResults);
+
+		for (let i = 1; i < filledInResults.length; i++) {
+			filledInResults[i] = fillMissingKeys(filledInResults[i], columnHeaders);
+		}
+
+		const resultsWithHeaders = [columnHeaders, ...filledInResults];
+
+		const columnWidths = Object.entries(firstRowResults).map(([key, value]) => {
+			const columnWidth =
+				typeof value === "number"
+					? Math.min(MAX_COL_WIDTH, Math.max(MIN_COL_WIDTH, String(value).length + 130))
+					: typeof value === "string"
+					? Math.min(MAX_COL_WIDTH, Math.max(MIN_COL_WIDTH, value.length + 130))
+					: MIN_COL_WIDTH;
+
+			return columnWidth;
+		});
+
+		return { columnWidths, columnCount, columnHeaders, resultsWithHeaders };
+	}, [props.results]);
+
+	const Cell = ({ columnIndex, rowIndex, style }) => {
+		const rowArray = Object.values(gridData.resultsWithHeaders[rowIndex]);
+		const value = rowArray[columnIndex];
+
 		return (
-			<tr>
-				{columnHeaders &&
-					columnHeaders.map(columnHeader => {
-						return (
-							<th
-								style={{
-									border: "1px solid darkgray",
-									padding: "3px 8px 3px 8px",
-								}}
-							>
-								{columnHeader}
-							</th>
-						);
-					})}
-			</tr>
+			<div
+				style={{
+					...style,
+					...cellStyle,
+					borderLeft: columnIndex === 0 ? "1px solid var(--base-border-color)" : "none",
+					borderTop: rowIndex === 0 ? "1px solid var(--base-border-color)" : "none",
+				}}
+			>
+				{value}
+			</div>
 		);
 	};
 
+	// @TODO make dynamic
+	let rowHeights = Array.from({ length: gridData.resultsWithHeaders.length }, () => MIN_ROW_HEIGHT);
+	// fixed header row height
+	if (rowHeights.length > 0) {
+		rowHeights[0] = 46;
+	}
 	return (
 		<>
-			<table style={{ width: "100%", borderCollapse: "collapse" }}>
-				<thead>{renderHeaderRow()}</thead>
-				<tbody>
-					{props.results.map(r => (
-						<NRQLResultsTableRow result={r} columnHeaders={columnHeaders} />
-					))}
-				</tbody>
-			</table>
+			{props.results && props.results.length > 0 && (
+				<>
+					{/* <Grid
+						columnCount={gridData.columnCount}
+						columnWidth={index => gridData.columnWidths[index]}
+						height={props.height}
+						rowCount={gridData.resultsWithHeaders.length}
+						rowHeight={index => rowHeights[index]}
+						width={props.width}
+					>
+						{Cell}
+					</Grid> */}
+					<GridWindow
+						columnCount={gridData.columnCount}
+						columnWidth={index => gridData.columnWidths[index]}
+						height={props.height}
+						rowCount={gridData.resultsWithHeaders.length}
+						rowHeight={index => rowHeights[index]}
+						width={props.width}
+					>
+						{Cell}
+					</GridWindow>
+				</>
+			)}
 		</>
 	);
 };
