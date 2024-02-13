f717a516421d9af14b4f863eb4712067406981ff
Calvin Allen <calvinallen@newrelic.com>
2024-02-13 16:21:37 -0500

    Attach query for surrounding logs / redraw table

--- shared/ui/Stream/APMLogging/APMLogSearchPanel.tsx
+++ shared/ui/Stream/APMLogging/APMLogSearchPanel.tsx
@@ -24,7 +24,7 @@ import Icon from "../Icon";
 import { Link } from "../Link";
 import { TableWindow } from "../TableWindow";
 import { APMLogRow } from "./APMLogRow";
-import { isEmpty as _isEmpty, isUndefined as _isUndefined } from "lodash";
+import { isEmpty as _isEmpty } from "lodash";
 
 interface SelectedOption {
 	value: string;
@@ -153,7 +153,10 @@ export const APMLogSearchPanel = (props: {
 	const [selectedEntityAccount, setSelectedEntityAccount] = useState<OptionProps | undefined>(
 		undefined
 	);
-	const [results, setResults] = useState<LogResult[]>([]);
+
+	const [originalSearchResults, setOriginalSearchResults] = useState<LogResult[]>([]);
+	const [searchResults, setSearchResults] = useState<LogResult[]>([]);
+
 	const [severityAttribute, setSeverityAttribute] = useState<string>();
 	const [messageAttribute, setMessageAttribute] = useState<string>();
 	const [currentShowSurroundingIndex, setCurrentShowSurroundingIndex] = useState<
@@ -161,8 +164,6 @@ export const APMLogSearchPanel = (props: {
 	>(undefined);
 	const [queriedWithNonEmptyString, setQueriedWithNonEmptyString] = useState<boolean>(false);
 	const [displayColumns, setDisplayColumns] = useState<string[]>([]);
-	const [beforeLogs, setBeforeLogs] = useState<LogResult[]>([]);
-	const [afterLogs, setAfterLogs] = useState<LogResult[]>([]);
 	const [surroundingLogsLoading, setSurroundingLogsLoading] = useState<boolean>();
 	const [totalItems, setTotalItems] = useState<number>(0);
 	const [logError, setLogError] = useState<string | undefined>("");
@@ -280,9 +281,7 @@ export const APMLogSearchPanel = (props: {
 	 */
 	const fetchSurroundingLogs = async (entityGuid: string, messageId: string, since: number) => {
 		try {
-			setSurroundingLogsLoading(true);
-			setBeforeLogs([]);
-			setAfterLogs([]);
+			setSearchResults([]);
 
 			const response = await HostApi.instance.send(GetSurroundingLogsRequestType, {
 				entityGuid,
@@ -302,13 +301,32 @@ export const APMLogSearchPanel = (props: {
 				return;
 			}
 
+			const surroundingLogs: LogResult[] = [];
+
 			if (response.beforeLogs && response.beforeLogs.length > 0) {
-				setResults(response.beforeLogs);
+				surroundingLogs.push(...response.beforeLogs);
 			}
 
+			const originalLog = searchResults.find(r => {
+				return r.messageId === messageId;
+			});
+
+			//this should ALWAYS be true, if not, bigger issue
+			surroundingLogs.push(originalLog!);
+
 			if (response.afterLogs && response.afterLogs.length > 0) {
-				setResults(response.afterLogs);
+				surroundingLogs.push(...response.afterLogs);
 			}
+
+			const logToPinIndex = surroundingLogs.findIndex(r => {
+				return r.messageId === messageId;
+			});
+			surroundingLogs[logToPinIndex] = {
+				...surroundingLogs[logToPinIndex],
+				isShowSurrounding: "true",
+			};
+			setCurrentShowSurroundingIndex(logToPinIndex);
+			setSearchResults(surroundingLogs);
 		} catch (ex) {
 			handleError(ex);
 		} finally {
@@ -321,10 +339,12 @@ export const APMLogSearchPanel = (props: {
 			setLogError(undefined);
 			setHasSearched(true);
 			setIsLoading(true);
-			setResults([]);
+			setSearchResults([]);
+			setOriginalSearchResults([]);
 			setSeverityAttribute(undefined);
 			setMessageAttribute(undefined);
 			setTotalItems(0);
+			setCurrentShowSurroundingIndex(undefined);
 
 			const filterText = suppliedQuery || query;
 
@@ -365,7 +385,8 @@ export const APMLogSearchPanel = (props: {
 			}
 
 			if (response.logs && response.logs.length > 0) {
-				setResults(response.logs);
+				setSearchResults(response.logs);
+				setOriginalSearchResults(response.logs);
 				setTotalItems(response.logs.length);
 				setMessageAttribute(response.messageAttribute!);
 				setSeverityAttribute(response.severityAttribute!);
@@ -450,35 +471,33 @@ export const APMLogSearchPanel = (props: {
 	};
 
 	const updateExpandedContent = (index, updatedJsx) => {
-		const newResults = [...results];
+		const newResults = [...searchResults];
 		newResults[index] = { ...newResults[index], expandedContent: updatedJsx };
-		setResults(newResults);
+		setSearchResults(newResults);
 	};
 
-	const updateShowSurrounding = index => {
-		const newResults = [...results];
+	const updateShowSurrounding = async (index: number, task: string) => {
+		if (task === "reset") {
+			const modifiedSearchResults = originalSearchResults.map(
+				({ isShowSurrounding, ...keepAttrs }) => keepAttrs
+			);
 
-		if (index === currentShowSurroundingIndex) {
-			newResults[index] = { ...newResults[index], isShowSurrounding: "false" };
-			setResults(newResults);
+			setSearchResults(modifiedSearchResults);
+			setOriginalSearchResults(modifiedSearchResults);
 			setCurrentShowSurroundingIndex(undefined);
 		} else {
-			if (!_isUndefined(currentShowSurroundingIndex)) {
-				newResults[currentShowSurroundingIndex] = {
-					...newResults[currentShowSurroundingIndex],
-					isShowSurrounding: "false",
-				};
-			}
-
-			newResults[index] = { ...newResults[index], isShowSurrounding: "true" };
-			setResults(newResults);
-			setCurrentShowSurroundingIndex(index);
+			const pinnedLog = searchResults[index];
+			await fetchSurroundingLogs(
+				selectedEntityAccount?.value,
+				pinnedLog.messageId,
+				parseInt(pinnedLog.timestamp)
+			);
 		}
 	};
 
 	const formatRowResults = () => {
-		if (results) {
-			let _results: LogResult[] = results;
+		if (searchResults) {
+			let _results: LogResult[] = searchResults;
 			// @TODO: eventually hook up "Show More"
 			// if (_results[_results.length - 1]?.showMore !== "true") {
 			// 	_results.push({ showMore: "true" });
@@ -625,12 +644,12 @@ export const APMLogSearchPanel = (props: {
 							TODO: Skeleton loader? Couldn't get it to work when I tried
 						)} */}
 
-					{!logError && !isLoading && results && totalItems > 0 && fieldDefinitions && (
+					{!logError && !isLoading && searchResults && totalItems > 0 && fieldDefinitions && (
 						<>
 							{ListHeader()}
 							<TableWindow
 								itemData={formatRowResults()}
-								itemCount={results.length}
+								itemCount={searchResults.length}
 								height={trimmedListHeight}
 								width={"100%"}
 							/>
