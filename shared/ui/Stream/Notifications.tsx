428be04a49ede69dc2c739978b3bca1014dfd1d1
Eric Jones <etjones319@gmail.com>
2024-06-18 08:47:56 -0700

    first pass at new notifications dialog

--- shared/ui/Stream/Notifications.tsx
+++ shared/ui/Stream/Notifications.tsx
@@ -1,83 +1,186 @@
 import { useAppDispatch, useAppSelector } from "@codestream/webview/utilities/hooks";
 import React, { useState } from "react";
 import { CodeStreamState } from "../store";
-import { isFeatureEnabled } from "../store/apiVersioning/reducer";
 import { Checkbox } from "../src/components/Checkbox";
 import { setUserPreference, closeModal } from "./actions";
-import {
-	CSNotificationDeliveryPreference,
-	CSNotificationPreference,
-} from "@codestream/protocols/api";
 import { Dialog } from "../src/components/Dialog";
+import { RadioGroup, Radio } from "../src/components/RadioGroup";
+import Icon from "./Icon";
 
 export const Notifications = props => {
 	const dispatch = useAppDispatch();
 	const derivedState = useAppSelector((state: CodeStreamState) => {
 		const hasDesktopNotifications = state.ide.name === "VSC" || state.ide.name === "JETBRAINS";
-		const notificationDeliverySupported = isFeatureEnabled(state, "notificationDeliveryPreference");
-		const emailSupported = isFeatureEnabled(state, "emailSupport");
-
 		return {
-			notificationPreference: state.preferences.notifications || CSNotificationPreference.InvolveMe,
-			notificationDeliveryPreference:
-				state.preferences.notificationDelivery || CSNotificationDeliveryPreference.All,
 			notifyPerformanceIssues: state.preferences.notifyPerformanceIssues === false ? false : true,
-			weeklyEmailDelivery: state.preferences.weeklyEmailDelivery === false ? false : true,
+			repoFollowingType: state.preferences.repoFollowingType
+				? state.preferences.repoFollowingType
+				: "AUTO",
+			serviceNotifyType: state.preferences.serviceNotifyType
+				? state.preferences.serviceNotifyType
+				: "REPO",
+			serviceNotifyTagValue: state.preferences.serviceNotifyTagValue
+				? state.preferences.serviceNotifyTagValue
+				: "",
+			serviceNotifyStringName: state.preferences.serviceNotifyStringName
+				? state.preferences.serviceNotifyStringName
+				: "",
+			serviceNotifyAccountId: state.preferences.serviceNotifyAccountId
+				? state.preferences.serviceNotifyAccountId
+				: "",
 			hasDesktopNotifications,
-			notificationDeliverySupported,
-			emailSupported,
 		};
 	});
 
-	const [loading, setLoading] = useState(false);
-	const [loadingDelivery, setLoadingDelivery] = useState(false);
-	const [loadingNotifyPerformanceIssues, setLoadingNotifyPerformanceIssues] = useState(false);
-	const [loadingWeeklyEmailDelivery, setLoadingWeeklyEmailDelivery] = useState(false);
-
-	const handleChange = async (value: string) => {
-		setLoading(true);
-		dispatch(setUserPreference({ prefPath: ["notifications"], value }));
-		setLoading(false);
-	};
-
-	const handleChangeWeeklyEmailDelivery = async (value: boolean) => {
-		setLoadingWeeklyEmailDelivery(true);
-		dispatch(setUserPreference({ prefPath: ["weeklyEmailDelivery"], value }));
-		setLoadingWeeklyEmailDelivery(false);
-	};
-
-	const handleChangeDelivery = async (value: string) => {
-		setLoadingDelivery(true);
-		dispatch(setUserPreference({ prefPath: ["notificationDelivery"], value }));
-		setLoadingDelivery(false);
-	};
+	const [serviceNotifyTagValue, setServiceNotifyTagValue] = useState(
+		derivedState.serviceNotifyTagValue
+	);
+	const [serviceNotifyStringName, setServiceNotifyStringName] = useState(
+		derivedState.serviceNotifyStringName
+	);
+	const [serviceNotifyAccountId, setServiceNotifyAccountId] = useState(
+		derivedState.serviceNotifyAccountId
+	);
 
 	const handleChangeNotifyPerformanceIssues = async (value: boolean) => {
-		setLoadingNotifyPerformanceIssues(true);
 		dispatch(setUserPreference({ prefPath: ["notifyPerformanceIssues"], value }));
-		setLoadingNotifyPerformanceIssues(false);
+	};
+
+	const handleChangeRepoFollowingType = async (value: string) => {
+		dispatch(setUserPreference({ prefPath: ["repoFollowingType"], value }));
+	};
+
+	const handleChangeServiceNotifyType = async (value: string) => {
+		dispatch(setUserPreference({ prefPath: ["serviceNotifyType"], value }));
+	};
+
+	const handleChangeServiceNotifyTagValue = async (value: string) => {
+		setServiceNotifyTagValue(value);
+		// dispatch(setUserPreference({ prefPath: ["serviceNotifyType"], value }));
+	};
+
+	const handleChangeServiceNotifyStringName = async (value: string) => {
+		setServiceNotifyStringName(value);
+		// dispatch(setUserPreference({ prefPath: ["serviceNotifyType"], value }));
+	};
+
+	const handleChangeServiceNotifyAccountId = async (value: string) => {
+		setServiceNotifyAccountId(value);
+		// dispatch(setUserPreference({ prefPath: ["serviceNotifyType"], value }));
 	};
 
 	return (
-		<Dialog title="Notification Settings" onClose={() => dispatch(closeModal())}>
+		<Dialog wide={true} title="Notification Settings" onClose={() => dispatch(closeModal())}>
 			<form className="standard-form vscroll">
 				<fieldset className="form-body">
 					<div id="controls">
-						{derivedState.hasDesktopNotifications && derivedState.notificationDeliverySupported && (
+						{derivedState.hasDesktopNotifications && (
 							<div>
-								<div style={{ marginTop: "20px" }}>
+								<div style={{ margin: "20px 0px 15px 0px" }}>
 									<Checkbox
 										name="notifyPerformanceIssues"
 										checked={derivedState.notifyPerformanceIssues}
 										onChange={handleChangeNotifyPerformanceIssues}
-										loading={loadingNotifyPerformanceIssues}
 									>
-										Notify me about performance issues
+										Notify me about services with performance problems
 									</Checkbox>
+									<div style={{ marginLeft: "25px" }} className="subtle">
+										CodeStream will email you about services associated with the selected
+										repositories that are exhibiting performance problems.
+									</div>
 								</div>
+								{derivedState.notifyPerformanceIssues && (
+									<>
+										<div style={{ fontSize: "larger", marginBottom: "10px" }} className="subtle">
+											REPOSITORIES YOU ARE FOLLOWING
+										</div>
+										<RadioGroup
+											name="repo-following-type"
+											selectedValue={derivedState.repoFollowingType}
+											onChange={value => handleChangeRepoFollowingType(value)}
+										>
+											<Radio value={"AUTO"}>Automatically follow any repository that I open</Radio>
+											<Radio value={"MANUAL"}>
+												Manually follow repositories
+												<div className="subtle">
+													Hover over a repository's name in the CodeStream tree view and click on
+													the Follow icon.
+												</div>
+											</Radio>
+										</RadioGroup>
+										{derivedState.repoFollowingType === "MANUAL" && (
+											<>
+												{/* @TODO: map through repos here */}
+												<div style={{ display: "flex", justifyContent: "space-between" }}>
+													<div>
+														<Icon name="repo" /> Repo Foo
+													</div>
+													<div>
+														<Icon className="clickable" name="x" />
+													</div>
+												</div>
+											</>
+										)}
+										<div
+											style={{ fontSize: "larger", margin: "15px 0px 10px 0px" }}
+											className="subtle"
+										>
+											SERVICES YOU WILL BE NOTIFIED ABOUT
+										</div>
+										<RadioGroup
+											name="service-notify-type"
+											selectedValue={derivedState.serviceNotifyType}
+											onChange={value => handleChangeServiceNotifyType(value)}
+										>
+											<Radio value={"REPO"}>All services for each repository</Radio>
+											<Radio value={"TAGVALUE"}>
+												All services with the following tag:value pairs
+											</Radio>
+											{derivedState.serviceNotifyType === "TAGVALUE" && (
+												<div style={{ marginBottom: "12px" }}>
+													<input
+														className="input-text control"
+														value={serviceNotifyTagValue}
+														onChange={event =>
+															handleChangeServiceNotifyTagValue(event.target.value)
+														}
+														placeholder="enviornment: production, enviornment: eu-production"
+													/>
+												</div>
+											)}
+											<Radio value={"STRINGNAME"}>
+												All services with the following string in the name
+											</Radio>
+											{derivedState.serviceNotifyType === "STRINGNAME" && (
+												<div style={{ marginBottom: "12px" }}>
+													<input
+														className="input-text control"
+														value={serviceNotifyStringName}
+														onChange={event =>
+															handleChangeServiceNotifyStringName(event.target.value)
+														}
+														placeholder="(Prod)"
+													/>
+												</div>
+											)}
+											<Radio value={"ACCOUNTID"}>All services in the following account IDs</Radio>
+											{derivedState.serviceNotifyType === "ACCOUNTID" && (
+												<div style={{ marginBottom: "12x" }}>
+													<input
+														className="input-text control"
+														value={serviceNotifyAccountId}
+														onChange={event =>
+															handleChangeServiceNotifyAccountId(event.target.value)
+														}
+														placeholder="1606862, 1693888"
+													/>
+												</div>
+											)}
+										</RadioGroup>
+									</>
+								)}
 							</div>
 						)}
-						<p>&nbsp;</p>
 					</div>
 				</fieldset>
 			</form>
